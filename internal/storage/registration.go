dc8b47db1efa777608a8018ca6a64674ccae0e4d
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-03-15 14:14:11 +0300

    update

--- internal/storage/registration.go
+++ internal/storage/registration.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Alexander1000/service-auth/internal/model"
 	"database/sql"
 	"fmt"
+	"strings"
 )
 
 func (r *Repository) Registration(ctx context.Context, userID int64, pass string, credentials []model.Credential) error {
@@ -26,6 +27,32 @@ func (r *Repository) Registration(ctx context.Context, userID int64, pass string
 			insert into users_pass(user_id, pass_hash, pass_salt, created_at)
 			values (%d, %s, %s, now())`,
 			userID,
+		),
+	)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	tuples := make([]string, 0, len(credentials))
+	for _, cred := range credentials {
+		tuples = append(
+			tuples,
+			fmt.Sprintf(
+				"(%d, %d, %d, now())",
+				userID,
+				cred.ID,
+				r.credTypeMap[cred.Type],
+			),
+		)
+	}
+
+	_, err = tx.ExecContext(
+		ctx,
+		fmt.Sprintf(`
+			insert into users_auth_credentials(user_id, credential_id, type_id, created_at)
+			values %s`,
+			strings.Join(tuples, ", "),
 		),
 	)
 	if err != nil {
--- internal/storage/repository.go
+++ internal/storage/repository.go
@@ -6,10 +6,12 @@ import (
 
 type Repository struct {
 	db *sql.DB
+	credTypeMap map[string]int
 }
 
 func New(db *sql.DB) *Repository {
 	return &Repository{
 		db: db,
+		credTypeMap: map[string]int{"login": 0, "email": 1, "phone": 2},
 	}
 }
