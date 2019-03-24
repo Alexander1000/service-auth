289683fd562a6aba68a4ceb1dc3247697648c254
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-03-24 12:22:09 +0300

    update

--- internal/storage/refresh.go
+++ internal/storage/refresh.go
@@ -2,8 +2,15 @@ package storage
 
 import (
 	"context"
+	"github.com/Alexander1000/service-auth/internal/model"
 )
 
-func (r *Repository) Refresh(ctx context.Context, token string) error {
-	return nil
+func (r *Repository) Refresh(ctx context.Context, token string) (*model.Token, error) {
+	conn, err := r.db.Conn(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+	
+	return nil, nil
 }
